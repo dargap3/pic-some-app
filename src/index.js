eb46df95c1fa09cbbfabac5baa7b2243881c31a9
dargap3 <dargap@hotmail.com>
2020-06-24 21:38:21 -0500

    installing react router and remixicon libraries, creatins header component and cart and photos pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'remixicon/fonts/remixicon.css';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router} from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
