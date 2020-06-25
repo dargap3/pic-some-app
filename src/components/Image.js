6c77b5023c1693c0ff81115c2dc9fa344bb2d7d6
dargap3 <dargap@hotmail.com>
2020-06-24 22:54:26 -0500

    adding favorite toggle to images and proptypes library

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+
+import { Context } from '../Context';
 
 function Image({className, img}) {
   const [hovered, setHovered] = useState(false);
+  const { toggleFavorite } = useContext(Context);
 
-  const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+  function heartIcon() {
+    if(img.isFavorite) {
+      return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>
+    } else if(hovered) {
+      return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+    }
+  }
+
   const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
 
   return (
@@ -13,10 +24,19 @@ function Image({className, img}) {
       onMouseLeave={() => setHovered(false)}
     >
       <img src={img.url} className="image-grid" alt=''/>
-      { heartIcon }
+      { heartIcon() }
       { cartIcon }
     </div>
   );
+}
+
+Image.propTypes = {
+  className: PropTypes.string,
+  img: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    isFavorite: PropTypes.bool
+  })
 }
 
 export default Image;
