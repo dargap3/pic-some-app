018b68e223bac42d4648232abd444e0393b78178
dargap3 <dargap@hotmail.com>
2020-06-26 11:52:03 -0500

    adding useHover custom hook

---
+++ src/hooks/useHover.js
@@ -0,0 +1,29 @@
+import {useState, useEffect, useRef} from "react";
+
+function useHover() {
+  const [hovered, setHovered] = useState(false);
+  const ref = useRef(null);
+  
+  function enter() {
+    setHovered(true)
+  }
+  
+  function leave() {
+    setHovered(false)
+  }
+  
+  useEffect(() => {
+    const currentRef = ref.current;
+    currentRef.addEventListener("mouseenter", enter)
+    currentRef.addEventListener("mouseleave", leave)
+    
+    return () => {    
+      currentRef.removeEventListener("mouseenter", enter)
+      currentRef.removeEventListener("mouseleave", leave)
+    }
+  }, []);
+  
+  return [hovered, ref]
+}
+
+export default useHover;
